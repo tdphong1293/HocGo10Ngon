cc7789639de02eb58ce6ee12ff4c705982c5a6db
tdphong1293 <ducphongtang1293@gmail.com>
2025-11-27 17:38:24 +0700

    feat: update server prisma client initialization (add adapter option)

--- server/src/modules/prisma/prisma.service.ts
+++ server/src/modules/prisma/prisma.service.ts
@@ -1,8 +1,18 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from 'src/generated/client';
+import { PrismaPg } from '@prisma/adapter-pg'
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+	constructor() {
+		super({
+			adapter: new PrismaPg({
+				connectionString: new ConfigService().get<string>('POSTGRES_URL') || '',
+			}),
+		});
+	}
+
 	async onModuleInit() {
 		await this.$connect();
 	}
