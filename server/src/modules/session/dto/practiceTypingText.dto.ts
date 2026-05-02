85c24f24f3f847581947c9276f4800fdd4bdf928
tdphong1293 <ducphongtang1293@gmail.com>
2026-05-02 08:16:33 +0700

    fix: add decorator and nested type  for mode property to comply with nestjs validation pipe

--- server/src/modules/session/dto/practiceTypingText.dto.ts
+++ server/src/modules/session/dto/practiceTypingText.dto.ts
@@ -1,10 +1,26 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsObject, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { SessionMode } from '../../mongoose/schemas/session_mode.schema';
+
+class PracticeSessionModeDto {
+    @IsString()
+    @IsNotEmpty({ message: 'Tên chế độ gõ không được để trống' })
+    modeName!: string;
+
+    @IsObject()
+    config!: Record<string, any>;
+
+    @IsObject()
+    @IsOptional()
+    subConfig?: Record<string, any>;
+}
 
 export class PracticeTypingTextDto {
     @IsString()
     @IsNotEmpty({ message: 'Mã ngôn ngữ không được để trống' })
-    languageCode: string;
+    languageCode!: string;
 
-    mode: SessionMode;
+    @ValidateNested()
+    @Type(() => PracticeSessionModeDto)
+    mode!: SessionMode;
 }
